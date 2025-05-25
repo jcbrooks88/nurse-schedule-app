d18399c3e826123fdcb392b5550cba023647e02f
UNC Coding Bootcamp <UNCBootcamp@JefferysMacBook.attlocal.net>
2025-05-25 11:55:41 -0400

    logout button

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,10 +1,12 @@
 import DashboardComponent from "../components/Dashboard";
+import { LogoutButton } from "../components/LogoutButton";
 
 
 export default function Dashboard() {
   return (
     <div>
         <DashboardComponent />
+        <LogoutButton />
     </div>
   );
 }
